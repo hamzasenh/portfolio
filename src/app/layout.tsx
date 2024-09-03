3b06825c328b907b16058c61ecdf32d620ca5a84
hamzasenh <hamzasenhaji0@gmail.comgit>
2024-09-03 14:35:32 +0200

    four commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Hamza Senhaji | Personal Portfolio',
-  description: 'Hamza is a Front-end Developer with 8 years of experience.',
+  description: 'Hamza is a Full-stack Developerpassionate about web development and UX/UI.',
 }
 
 export default function RootLayout({
