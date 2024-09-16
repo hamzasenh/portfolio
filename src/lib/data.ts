c80d3f777e3a2fe2ab4b49231aa68047f2d52656
hamzasenh <hamzasenhaji0@gmail.comgit>
2024-09-17 01:48:44 +0200

    17_09_2024_2

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -58,7 +58,7 @@ export const projectsData = [
     title: "To-do it!",
     description:
       "Creation of a simple To-Do List application built with React and Vite. It allows users to add, view, and delete tasks in an intuitive way.",
-    tags: ["React", "Vite.JS"],
+    tags: ["React", "Vite.JS", "CSS"],
     imageUrl: todo,
     url: "https://todoit-hs.vercel.app/",
   },
