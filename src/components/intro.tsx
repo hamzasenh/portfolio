9daecc7b1c34e13f6e24eafcac284a497f2319ff
hamzasenh <hamzasenhaji0@gmail.comgit>
2024-10-21 09:07:38 +0200

    ajout new

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -63,7 +63,7 @@ export default function Intro() {
             <span className='font-bold'>Hello, I&apos;m 
             Hamza.</span> I&apos;m a {" "} 
             <span className='font-bold'>full-stack
-            Developer</span>{" "} I enjoy 
+            Developer & UI Designer.</span>{" "} I enjoy 
             building <span className='italic'>sites & 
             apps</span>. My focus is{" "}
             <span className='underline'>React (Next.js)</span>
