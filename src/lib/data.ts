cd46698ce990da9e898c083d1827ed4f5475a251
hamzasenh <hamzasenhaji0@gmail.com>
2025-10-10 19:41:07 +0200

    10102025_2

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -57,6 +57,7 @@ export const experiencesData = [
   {
     title: "IT Support at Mission Locale de Molenbeek",
     location: "Brussels",
+    description:"",
     icon: React.createElement(CgWorkAlt),
     date: "Jan 2025 - Currently",
   },
